e7901041f87a432cc4f74b98aa6541636d01dea5
linhfishCR7 <59117535+linhfishCR7@users.noreply.github.com>
2020-01-05 09:34:23 +0700

    thêm ngày hôm nay ví dụ

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
-echo'HỌC WEB PHP';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TRANG CHỦ</title>
+</head>
+<body>
+
+    <?php
+        echo'HỌC WEB PHP';
+        echo'Hôm nay là:' .date(y/m/d);
+    ?>
+</body>
+</html>
